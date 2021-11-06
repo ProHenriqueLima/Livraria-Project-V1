f116da6d0b2fe992ceef0db80f6c8b7cdecad502
ProHenriqueLima <prohenriquelima@gmail.com>
2021-11-05 22:26:49 -0300

    Atualizações de hoje

--- Livraria.FrontEnd/frontvue/src/views/Home.vue
+++ Livraria.FrontEnd/frontvue/src/views/Home.vue
@@ -14,11 +14,11 @@
  
       <v-card
       class=" mt-6 mv-2 grey darken-4 text-center mx-auto"
-      width="1080"
+ 
       height="50"
     >
       <h1 class="white--text mt-1 mb-1 grey darken-4 ">
-        Ultimo Aluguel Feito 
+        DashBoard
       </h1>
     </v-card>
       <v-row class="mx-auto">
@@ -30,9 +30,7 @@
         >
           <div>
             <v-card-title class="ml-3 white--text text-center"
-              ><h3>
-                Número de Alugueis Feitos
-              </h3>
+              ><v-icon class="white--text mr-2">mdi-briefcase</v-icon> Alugueis
             </v-card-title>
           </div>
           <div>
@@ -41,46 +39,105 @@
             </h1>
           </div>
         </v-card>
-
         <v-card
           :loading="loading"
-          class=" my-5 deep-purple darken-3 text-center"
+          class=" my-5 ml-4 mr-4 deep-purple darken-3 text-center"
           width="350"
           height="150"
         >
           <div>
-            <v-card-title class=" white--text text-center mx-4"
-              ><h3>Último Cliente Que Alugou</h3>
+            <v-card-title class="ml-3 white--text text-center"
+              > <v-icon class="white--text mr-2">mdi-account</v-icon> Clientes
             </v-card-title>
           </div>
           <div>
             <h1 class=" text-center white--text ">
-              <b>{{ this.Alugueis[Alugueis.length - 1].cliente.nomeCliente }}</b>
+              <b>{{ this.Clientes.length }}</b>
             </h1>
-          </div>
+          </div>     
         </v-card>
-
         <v-card
           :loading="loading"
-          class=" my-5 mx-4 deep-purple darken-3 text-center"
+          class=" my-5 ml-4 mr-4 deep-purple darken-3 text-center"
           width="350"
           height="150"
         >
           <div>
-            <v-card-title class=" white--text text-center mx-4"
-              ><h3>O Último Livro Alugado</h3>
+            <v-card-title class="ml-3 white--text text-center"
+              > <v-icon class="white--text mr-2">mdi-book-open-page-variant</v-icon> Livros
             </v-card-title>
           </div>
           <div>
             <h1 class=" text-center white--text ">
-              <b>{{ this.Alugueis[Alugueis.length - 1].livro.nomeLivro }}</b>
+              <b>{{ this.Livros.length }}</b>
             </h1>
           </div>
         </v-card>
       </v-row>
+      <v-card
+      class="  purple darken-3 text-center mx-auto"
+      width="1120"
+      height="50"
+    >
+      <h1 class="white--text mt-1 font-weight-black">
+        Ultimo Aluguel Feito
+      </h1>
+    </v-card>
+    <v-row class="mx-auto mb-5">
+      <v-card
+        class=" mt-5 purple darken-3 text-center mr-4"
+        width="350"
+        height="150"
+      >
+        <div>
+          <v-card-title class=" white--text text-center"
+            ><h3>Cliente</h3>
+          </v-card-title>
+        </div>
+        <div>
+          <h1 class=" text-center white--text ">
+            <b>{{ ultimoAluguel.cliente.nomeCliente }}</b>
+          </h1>
+        </div>
+      </v-card>
+
+      <v-card
+        class=" my-5 purple darken-3 text-center"
+        width="350"
+        height="150"
+      >
+        <div>
+          <v-card-title class=" white--text text-center mx-4"
+            ><h3>Livro</h3>
+          </v-card-title>
+        </div>
+        <div>
+          <h1 class=" text-center white--text ">
+            <b>{{ ultimoAluguel.livro.nomeLivro }}</b>
+          </h1>
+        </div>
+      </v-card>
+
+      <v-card
+        class="ml-3 my-5 purple darken-3 text-center ml-4"
+        width="350"
+        height="150"
+      >
+        <div>
+          <v-card-title class=" white--text text-center mx-4"
+            ><h3>Data do Aluguel</h3>
+          </v-card-title>
+        </div>
+        <div>
+          <h1 class=" text-center white--text ">
+            <b>{{ ultimoAluguel.data_Aluguel }}</b>
+          </h1>
+        </div>
+      </v-card>
+    </v-row>
   
     <hr>
-    <graficos /> 
+     <graficos /> 
 
     
   </v-app>
@@ -89,17 +146,19 @@
 <script>
 import NavBar from "../components/NavBar.vue";
 import Alugueis from "../services/Alugueis.js";
-import Livro from "../services/Livros";
-import Graficos from "../components/Graficos"
+import Clientes from "../services/Clientes.js";
+import Livros from "../services/Livros";
+ import Graficos from "../components/Graficos"
 export default {
   name: "Home",
   components: {
-    NavBar,Graficos
+     NavBar,Graficos
   },
   data() {
     return {
       Alugueis: [],
       Livros: [],
+      Clientes:[],
     };
   },
   mounted() {
@@ -109,11 +168,13 @@ export default {
     listar() {
       Alugueis.listar().then((resposta) => {
         (this.Alugueis = resposta.data), console.log(this.Alugueis);
+        this.ultimoAluguel=this.Alugueis[this.Alugueis.length - 1];
       });
-    },
-    listarLivros() {
-      Livro.listar().then((resposta) => {
+      Livros.listar().then((resposta) => {
         this.Livros = resposta.data;
+      });
+      Clientes.listar().then((resposta) => {
+        this.Clientes = resposta.data;
       });
     },
   },
